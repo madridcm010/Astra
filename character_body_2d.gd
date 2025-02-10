eec3cd3c266569aeb53b0bbc1bed7b011f26bbe8
madridcm010 <snowfallslower@gmail.com>
2025-02-10 14:13:49 -0500

    first commit

---
+++ character_body_2d.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var speed = 400
+const rotation_speed = 180
+
+#input that player gives
+
+var _direction : Vector2
+func _move(direction : Vector2):
+	_direction = direction
+	
+func get_input():
+	var input_direction = Input.get_vector("left","right","up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("flip"):
+		$Sprite2D.rotation_degrees += 180
+		$CollisionShape2D.rotation_degrees += 180
+	get_input()
+	move_and_slide()
