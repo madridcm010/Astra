01cf0a6f24be452e8b9ca8c21838fa7ead0bed2e
madridcm010 <snowfallslower@gmail.com>
2025-03-19 02:54:09 -0400

    healthbar is working

--- Scenes/boss/boss.gd
+++ Scenes/boss/boss.gd
@@ -2,21 +2,33 @@ extends AnimatedSprite2D
 #enum collections for phases of boss and for bullet patterns.
 enum patterns { spread = 1 , tracer = 2, box = 3 , circle = 4 }
 enum phases { phase1 = 1, phase2 = 2, phase3 = 3 }
-@export var Health = 100
+@export var hp = 100.0 : set = _set_health
 @export var speed = 0.0
 @export var phase: phases
 @export var bullet_patterns: patterns
+@export var _damage = 0.25 : set = change_hp
 @onready var healthbar = $healthbar
 
+func _set_health(value: float):
+	hp = value 
+	#if hp<=0:
+		#_die()
+	healthbar.hp = hp
 
+func change_hp(_damage: float):
+	#healthbar.value = hp
+	_set_health(hp - _damage)
+	
 
 func _ready():
-	var hp = Health
 	healthbar.init_health(hp)
+	
 func _process(delta: float) -> void:
 	get_input()
 
 func get_input():
-	if Input.is_action_pressed("shoot"):
-		healthbar._set_hp(90)
+	#for i in range(0, hp + 1):
+		if Input.is_action_just_pressed("shoot"):
+			#_set_health(hp - _damage)
+			change_hp(_damage)
 		
