caf718ac1d4699d3726c65feda1c86fa83990133
SamuraiPhunk <elliott.schmidt22@gmail.com>
2025-05-02 03:26:41 -0400

    Hub 90% Complete
    Just so many updates
    -Added UI for ship purchasing
    -Added UI for upgrade purchasing
    -Added logic for implemented UI
    -Added HubStats resource
    -Added numerous asset files
    -Many more

---
+++ Resources/Hub/Hub_Stats.gd
@@ -0,0 +1,20 @@
+class_name HubStats
+extends Resource
+
+
+@export var health_cost : int
+@export var thruster_cost : int
+@export var attackspeed_cost : int 
+@export var damage_cost : int
+@export var ship1_cost : int
+@export var ship2_cost : int
+@export var ship3_cost : int
+
+@export var health_level : int
+@export var thruster_level : int
+@export var attackspeed_level : int
+@export var damage_level : int
+
+@export var ship1purchased : bool
+@export var ship2purchased : bool
+@export var ship3purchased : bool
