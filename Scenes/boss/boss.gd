c68df987c3a50ac020b85cc5c52353559fed5a74
madridcm010 <snowfallslower@gmail.com>
2025-05-02 20:16:59 -0400

    boss collision

--- Scenes/boss/boss.gd
+++ Scenes/boss/boss.gd
@@ -14,7 +14,7 @@ signal boss_defeated
 
 func _set_health(value: float):
 	if hp<=0:
-		$"../CollisionShape2D".disabled
+		$"../Area2D".disconnect("area_entered" , _on_area_2d_area_entered)
 		boss_defeated.emit()
 		await get_tree().create_timer(1).timeout
 		_dead()
@@ -35,20 +35,19 @@ func _ready():
 	healthbar.init_health(hp)
 	_Idle()
 	
-func _process(delta: float) -> void:
-	get_input()
+#func  _process(delta: float) -> void:
+	#get_input()
 
-func get_input():
-	#for i in range(0, hp + 1):
-	if Input.is_action_just_pressed("shoot"):
-		#_set_health(hp - _damage)
-		
-		play("damage")
-		change_hp(_damage)
-		await get_tree().create_timer(.5).timeout
-		_Idle()
-		if hp<0:
-			change_hp(_damage)
+#func get_input():
+	##for i in range(0, hp + 1):
+		##_set_health(hp - _damage)
+		#
+		#play("damage")
+		#change_hp(_damage)
+		#await get_tree().create_timer(.5).timeout
+		#_Idle()
+		#if hp<0:
+			#change_hp(_damage)
 			
 			
 		
@@ -63,3 +62,13 @@ func _dead():
 func _Idle():
 	if hp != 0:
 		play("Idle")
+
+# boss damage
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet") and !_dead():
+		play("damage")
+		change_hp(_damage)
+		await get_tree().create_timer(.5).timeout
+		_Idle()
+		if hp<0:
+			change_hp(_damage)
